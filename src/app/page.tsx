0326259c5599e7bb80f1de1752670979bb9c73a5
shlee <aoao1023@gmail.com>
2024-10-03 12:58:25 +0900

    Feat main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,20 @@
-import Image from "next/image";
+import H1 from '../components/typography/h1'
+import P from '../components/typography/p'
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
 
 export default function Home() {
-  return <div>메인</div>;
+  return (
+    <div className="h-full flex flex-col justify-around px-4">
+      <div className="flex flex-col gap-4 items-center justify-center">
+        <H1>Fooool Baal</H1>
+        <P className="text-center">
+          Keep track of your favorite team schedule and game results
+        </P>
+      </div>
+      <Link href={'/explore'}>
+        <Button className="w-full">Explore</Button>
+      </Link>
+    </div>
+  )
 }
